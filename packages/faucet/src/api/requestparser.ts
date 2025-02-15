5699e43c03ee8abc2243daf8dde52ee82a20731a
filipdjokic <djokicf@protonmail.com>
2025-02-15 13:07:27 +0100

    Fix requestparser type checks

--- packages/faucet/src/api/requestparser.ts
+++ packages/faucet/src/api/requestparser.ts
@@ -51,7 +51,7 @@ export class RequestParser {
       throw new HttpError(400, "Property 'denom' must not be empty.");
     }
     
-    if (email && typeof email !== "string") {
+    if (typeof email !== "string") {
       throw new HttpError(400, "Property 'email' must be a string.");
     }
 
@@ -63,15 +63,15 @@ export class RequestParser {
       throw new HttpError(400, "Property 'amount' must be a number.");
     }
 
-    if (marketingOptin && typeof marketingOptin !== "boolean") {
+    if (typeof marketingOptin !== "boolean") {
       throw new HttpError(400, "Property 'marketing_optin' must be a boolean.");
     }
     
-    if (name &&typeof name !== "string") {
+    if (typeof name !== "string") {
       throw new HttpError(400, "Property 'name' must be a string.");
     }
 
-    if (typeof company !== "string") {
+    if (company && typeof company !== "string") {
       throw new HttpError(400, "Property 'company' must be a string.");
     }
 
